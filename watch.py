eba72314f19d24f6d58c66ca1003365311673d94
Jared Hanisch <jared.hanisch@gmail.com>
2021-07-06 20:55:59 -0500

    Added update flag to not send empty email if no watch is found

--- watch.py
+++ watch.py
@@ -13,6 +13,8 @@ reddit_client_id = os.environ.get('REDDIT_CLIENT_ID')
 reddit_client_secret = os.environ.get('REDDIT_CLIENT_SECRET')
 reddit_user_agent = os.environ.get('REDDIT_USER_AGENT')
 
+#flag for when update occurs
+update = False
 #list of watches you want to be notified for
 keywords = ["BREW", "VOSTOK"]
 #initializing the email contents, setting the subject
@@ -31,11 +33,13 @@ for submission in reddit.subreddit("watchexchange").new():
         #if the submission was created within the last hour and the title contains one of the keywords
         if((time.time() - submission.created_utc < 3600) and (submission.title.upper().find(keyword) != -1)):
             #appending all watches that match what I'm looking for to the body of the email
-            message += "reddit.com"+submission.permalink+"\n" 
+            message += "reddit.com"+submission.permalink+"\n"
+            update = True
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login(sender_email, sender_password)
-    server.sendmail(sender_email, destination_email, message)
+    if update:
+        server.sendmail(sender_email, destination_email, message)
     
